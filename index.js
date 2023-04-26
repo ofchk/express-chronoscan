2aa1b9c34f1e0ea4a9306bd0f3a0926b0c40f9fa
sarah <sarah@ofchk.biz>
2023-04-26 14:57:46 +0530

    add AlfrescoAPI 4

--- index.js
+++ index.js
@@ -73,7 +73,8 @@ app.post('/alfresco/upload', upload.single('file'), async(req, res) => {
 
 app.post('/invoice/upload', upload.array('file', 4), async(req, res) => {
     console.log("files", req.files)
-    console.log("file",req.file)
+    console.log("length",req.files)
+    console.log("body",req.body)
 
     var AlfrescoApi = require('alfresco-js-api-node');
     var alfrescoJsApi = new AlfrescoApi({ provider:'ECM', hostEcm: 'http://alfresco.moc.com:8080' });
