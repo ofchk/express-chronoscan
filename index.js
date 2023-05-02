f35d90a9415624705f0ba4bcb80837e4a00af099
sarah <sarah@ofchk.biz>
2023-05-02 16:53:40 +0530

    updates

--- index.js
+++ index.js
@@ -38,58 +38,57 @@ const upload = multer({ storage: storage })
 app.use(cors())
 
 
-app.post('/alfresco/upload', upload.single('file'), async(req, res) => {
-        var AlfrescoApi = require('alfresco-js-api-node');
+// app.post('/alfresco/upload', upload.single('file'), async(req, res) => {
+//         var AlfrescoApi = require('alfresco-js-api-node');
 
-        var alfrescoJsApi = new AlfrescoApi({ provider:'ECM', hostEcm: 'http://alfresco.moc.com:8080' });
+//         var alfrescoJsApi = new AlfrescoApi({ provider:'ECM', hostEcm: 'http://alfresco.moc.com:8080' });
 
-        alfrescoJsApi.login("admin", "admin").then(function (data) {
-          console.log('API called successfully to login into Alfresco Content Services.');
-        }, function (error) {
-          console.error('Trap error',error);
-        });
+//         alfrescoJsApi.login("admin", "admin").then(function (data) {
+//           console.log('API called successfully to login into Alfresco Content Services.');
+//         }, function (error) {
+//           console.error('Trap error',error);
+//         });
 
-        var fs = require('fs');
-        var result = [];
+//         var fs = require('fs');
+//         var result = [];
         
-        var fileToUpload = fs.createReadStream(req.file.path);
-        await alfrescoJsApi.upload.uploadFile(fileToUpload,'ChronoscanInvoices')
-        .then(function (response) {
-            const nodeid = response.entry.id;
-            const filename = response.entry.name;
-            console.log('File Uploaded in the ChronoscanInvoices');
-            //var previewUrl = alfrescoJsApi.content.getDocumentPreviewUrl(response.entry.id);
-                              var contentUrl = alfrescoJsApi.content.getContentUrl(response.entry.id);
-            console.log(contentUrl)
-            result.push({'status': 200, 'filename': filename, 'nodeid': nodeid, 'contentUrl': contentUrl, 'message': 'File - '+filename+' uploaded successfully' });
-        }, function (error) {
-            console.log('errorkey', JSON.parse(error.response.text).error.errorKey)
-            console.log('Error during the upload' + error);
-            //result.push(error);
-            result.push({'status': 409, 'message': JSON.parse(error.response.text).error.errorKey });
-        });
-        res.json(result);
-    });
+//         var fileToUpload = fs.createReadStream(req.file.path);
+//         await alfrescoJsApi.upload.uploadFile(fileToUpload,'ChronoscanInvoices')
+//         .then(function (response) {
+//             const nodeid = response.entry.id;
+//             const filename = response.entry.name;
+//             console.log('File Uploaded in the ChronoscanInvoices');
+//             //var previewUrl = alfrescoJsApi.content.getDocumentPreviewUrl(response.entry.id);
+//                               var contentUrl = alfrescoJsApi.content.getContentUrl(response.entry.id);
+//             console.log(contentUrl)
+//             result.push({'status': 200, 'filename': filename, 'nodeid': nodeid, 'contentUrl': contentUrl, 'message': 'File - '+filename+' uploaded successfully' });
+//         }, function (error) {
+//             console.log('errorkey', JSON.parse(error.response.text).error.errorKey)
+//             console.log('Error during the upload' + error);
+//             //result.push(error);
+//             result.push({'status': 409, 'message': JSON.parse(error.response.text).error.errorKey });
+//         });
+//         res.json(result);
+//     });
 
 app.post('/invoice/upload', upload.array('file', 4), async(req, res) => {
-    console.log("files", req.files)
-    console.log("length",req.files)
-    console.log("body",req.body.invoice)
-
+    const invoice_number = req.body.invoice;
     var AlfrescoApi = require('alfresco-js-api-node');
+
     var alfrescoJsApi = new AlfrescoApi({ provider:'ECM', hostEcm: 'http://alfresco.moc.com:8080' });
 
     alfrescoJsApi.login('admin', 'admin').then(function (data) {
       console.log('API called successfully to login into Alfresco Content Services.');
     }, function (error) {
       console.error(error);
+      res.json({ 'status' : 400, 'message': "Error in Alfresco Connection." })
     });
 
       var fs = require('fs');
       var result = [];
       for(var i = 0; i < req.files.length; i++){
         var fileToUpload = fs.createReadStream(req.files[i].path);
-        await alfrescoJsApi.upload.uploadFile(fileToUpload,'ChronoscanInvoices/'+req.body.invoice)
+        await alfrescoJsApi.upload.uploadFile(fileToUpload,'ChronoscanInvoices/'+invoice_number)
         .then(function (response) {
             const nodeid = response.entry.id;
             const filename = response.entry.name;
@@ -97,7 +96,7 @@ app.post('/invoice/upload', upload.array('file', 4), async(req, res) => {
             //var previewUrl = alfrescoJsApi.content.getDocumentPreviewUrl(response.entry.id);
             var contentUrl = alfrescoJsApi.content.getContentUrl(response.entry.id);
             console.log(contentUrl)
-            result.push({'status': 200, 'entryid': response.entry.id, 'filename': filename, 'nodeid': nodeid, 'contentUrl': contentUrl, 'message': 'File - '+filename+' uploaded successfully' });
+            result.push({'status': 200, 'invoice_number': invoice_number, 'entryid': response.entry.id, 'filename': filename, 'nodeid': nodeid, 'contentUrl': contentUrl, 'message': 'File - '+filename+' uploaded successfully' });
         }, function (error) {
             console.log('errorkey', JSON.parse(error.response.text).error.errorKey)
             console.log('Error during the upload' + error);
@@ -107,8 +106,8 @@ app.post('/invoice/upload', upload.array('file', 4), async(req, res) => {
       }
       
       res.json(result)
-
-    });
+          
+});
 
 
 
