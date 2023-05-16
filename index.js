88cf9dc02fbadbee3f3728f6f4cfdcb47f84c8e2
sarah <sarah@ofchk.biz>
2023-05-16 15:11:38 +0530

    Error: NJS-059 fix 2 test 10

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ async function connect_oracle_staging(invoice_number, invoice_id, vendor_name, s
 //    console.log("Number of rows inserted:", result);
 
    sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, GL_DATE, OPERATING_UNIT, ENTERED_AMOUNT) VALUES (:1,:2,:3,:4,:5,:6,:7)`;
-   
+
 
     binds = [
       [ invoice_number, vendor_name, site_id, currency, gl_date, entity_name, amount ]
@@ -132,7 +132,7 @@ console.log(sql)
     }
   }
 }
-connect_oracle_staging("StagingSample444" ,"Al NahlaSolutions LLC 98765","OMR", 106, "2023-05-10T00:00:00.000Z","Muscat Overseas Engineering LLC", 1357, "http://alfresco.moc.com:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/c18aee25-4b3b-4e19-844f-458d158ea24c/content?attachment=false&alf_ticket=TICKET_2e1c58da2669bbe5f87a79492c259afaca3bdde8")
+connect_oracle_staging("StagingSample444" ,"Al NahlaSolutions LLC 98765","OMR", 106, 2023-05-10T00:00:00.000Z,"Muscat Overseas Engineering LLC", 1357, "http://alfresco.moc.com:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/c18aee25-4b3b-4e19-844f-458d158ea24c/content?attachment=false&alf_ticket=TICKET_2e1c58da2669bbe5f87a79492c259afaca3bdde8")
 
 function doc_dicer(itemPath){
   try {            
