5505acf1b3f315e1e23176b9beab6f59420fbdcb
sarah <sarah@ofchk.biz>
2023-05-16 17:37:26 +0530

    Type Mismatch

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
 //    result = await connection.execute(sql);
 //    console.log("Number of rows inserted:", result);
 
-   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, ATTRIBUTE9, GL_DATE, INVOICE_DATE) VALUES (:1,:2,:3,:4,:5,:6,:7,:7)`;
+   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE) VALUES (:1,:2,:3,:4,:5,:6,:7)`;
 
 
     binds = [
@@ -111,7 +111,6 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
         { type: oracledb.STRING, maxSize: 200 },
         { type: oracledb.STRING, maxSize: 200 },
         { type: oracledb.NUMBER },
-        { type: oracledb.STRING, maxSize: 200 },
         { type: oracledb.STRING, maxSize: 200 }
       ]
     };
