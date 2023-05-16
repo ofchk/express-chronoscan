feed2562c5bbdd9e2a4daccc9f71649722de31a1
sarah <sarah@ofchk.biz>
2023-05-16 15:20:20 +0530

    Error: NJS-059 fix 2 test 13

--- index.js
+++ index.js
@@ -79,7 +79,10 @@ function save_staging(
     });
 }
 
-async function connect_oracle_staging(invoice_number, vendor_name, site_id, currency, gl_date, entity_name, amount, contentUrl) {
+
+
+async function connect_oracle_staging(invoice_number, vendor_name, site_id, currency, entity_name, amount, contentUrl) {
+
   let connection;
 
   try {
@@ -91,11 +94,11 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
 //    result = await connection.execute(sql);
 //    console.log("Number of rows inserted:", result);
 
-   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, GL_DATE, OPERATING_UNIT, ENTERED_AMOUNT, ATTRIBUTE9) VALUES (:1,:2,:3,:4,:5,:6,:7,:8)`;
+   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, ATTRIBUTE9) VALUES (:1,:2,:3,:4,:5,:6,:7)`;
 
 
     binds = [
-      [ invoice_number, vendor_name, site_id, currency, gl_date, entity_name, amount, contentUrl ]
+      [ invoice_number, vendor_name, site_id, currency, entity_name, amount, contentUrl ]
     ];
 
     options = {
@@ -106,7 +109,6 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
         { type: oracledb.STRING, maxSize: 200 },
         { type: oracledb.NUMBER },
         { type: oracledb.STRING, maxSize: 200 },
-        { type: oracledb.DATE},
         { type: oracledb.STRING, maxSize: 200 },
         { type: oracledb.NUMBER },
         { type: oracledb.STRING, maxSize: 300 }
@@ -133,7 +135,9 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
     }
   }
 }
-connect_oracle_staging("StagingSample444" ,"Al NahlaSolutions LLC 98765", 106, "OMR", "2023-05-10T00:00:00.000Z","Muscat Overseas Engineering LLC", 1357, "http://alfresco.moc.com:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/c18aee25-4b3b-4e19-844f-458d158ea24c/content?attachment=false&alf_ticket=TICKET_2e1c58da2669bbe5f87a79492c259afaca3bdde8")
+
+connect_oracle_staging("StagingSample444" ,"Al NahlaSolutions LLC 98765", 106, "OMR","Muscat Overseas Engineering LLC", 1357, "http://alfresco.moc.com:8080/alfresco/api/-default-/public/alfresco/versions/1/nodes/c18aee25-4b3b-4e19-844f-458d158ea24c/content?attachment=false&alf_ticket=TICKET_2e1c58da2669bbe5f87a79492c259afaca3bdde8")
+
 
 function doc_dicer(itemPath){
   try {            
