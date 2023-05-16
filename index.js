3f9fdece24de98602c7306b04fe08d0c292e79d5
sarah <sarah@ofchk.biz>
2023-05-16 17:31:42 +0530

    Error: NJS-059 fix 2 test 24

--- index.js
+++ index.js
@@ -94,11 +94,11 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
 //    result = await connection.execute(sql);
 //    console.log("Number of rows inserted:", result);
 
-   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE) VALUES (:1,:2,:3,:4,:5,:6,:7,:7)`;
+   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, GL_DATE) VALUES (:1,:2)`;
 
 
     binds = [
-      [ invoice_number, vendor_name, site_id, currency, entity_name, amount, "16-MAY-23" ]
+      [ invoice_number, "16-MAY-23" ]
     ];
 
     options = {
@@ -106,12 +106,7 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
       // batchErrors: true,  // continue processing even if there are data errors
       bindDefs: [
         { type: oracledb.STRING, maxSize: 200 },
-        { type: oracledb.STRING, maxSize: 200 },
-        { type: oracledb.NUMBER },
-        { type: oracledb.STRING, maxSize: 200 },
-        { type: oracledb.STRING, maxSize: 200 },
-        { type: oracledb.NUMBER },
-        { type: oracledb.DATE },
+        { type: oracledb.DATE }
       ]
     };
 
