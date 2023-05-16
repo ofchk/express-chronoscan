b60f2dc3b12dda7755c6b0d60d8fb5eb19a1c730
sarah <sarah@ofchk.biz>
2023-05-16 14:57:01 +0530

    Error: NJS-059 fix 2 test 5

--- index.js
+++ index.js
@@ -91,11 +91,11 @@ async function connect_oracle_staging(invoice_number, invoice_id, vendor_name, s
 //    result = await connection.execute(sql);
 //    console.log("Number of rows inserted:", result);
 
-   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, GL_DATE, OPERATING_UNIT, ENTERED_AMOUNT, ATTRIBUTE9) VALUES (:1,:2,:3,:4,:5,:6,:7,:8)`;
+   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, GL_DATE, OPERATING_UNIT, ENTERED_AMOUNT, ATTRIBUTE9) VALUES (invoice_number, vendor_name, site_id, currency, gl_date, entity_name, amount, contentUrl )`;
 
 
     binds = [
-      [ invoice_number, vendor_name, site_id, currency, gl_date, entity_name, amount, contentUrl ]
+      [ ]
     ];
 
     options = {
