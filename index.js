e3a5550ef5defd443afdbd8dd221ea59885bd19a
sarah <sarah@ofchk.biz>
2023-05-17 16:03:52 +0530

    oracle returning RESULT ROW

--- index.js
+++ index.js
@@ -94,9 +94,8 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
 //    result = await connection.execute(sql);
 //    console.log("Number of rows inserted:", result);
 
-   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE, ATTRIBUTE9) VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9, :10) RETURNING RECORD_ID INTO :RECORD_ID`;
+   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE, ATTRIBUTE9) VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9, :10)`;
 
-await conn('LAST_HISTORY_ID').insert({'HISTORY_ID': firstRecord.historyId}).returning('ID');
 
 
     binds = [
