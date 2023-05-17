98c242aa32715e3cd010bb54d5ada58093d3b88a
sarah <sarah@ofchk.biz>
2023-05-17 14:58:11 +0530

    oracle returning id added ROECORD_ID error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 var throttle = require('express-throttle-bandwidth');
-
+var cron = require('node-cron');
 const oracledb = require('oracledb');
 
 const dbConfig = require('./dbconfig.js');
@@ -94,11 +94,11 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
 //    result = await connection.execute(sql);
 //    console.log("Number of rows inserted:", result);
 
-   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE, ATTRIBUTE9) VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9) `;
+   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE, ATTRIBUTE9, RECORD_ID) VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9, :10) RETURNING RECORD_ID INTO staging_id`;
 
 
     binds = [
-      [ invoice_number, vendor_name, site_id, currency, entity_name, amount, gl_date, gl_date, contentUrl ]
+      [ invoice_number, vendor_name, site_id, currency, entity_name, amount, gl_date, gl_date, contentUrl, 1 ]
     ];
 
     options = {
@@ -113,7 +113,9 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
         { type: oracledb.NUMBER },
         { type: oracledb.STRING, maxSize: 200 },
         { type: oracledb.STRING, maxSize: 200 },
-        { type: oracledb.STRING, maxSize: 1000 }
+        { type: oracledb.STRING, maxSize: 1000 },
+        { type: oracledb.NUMBER }
+
       ]
     };
 
