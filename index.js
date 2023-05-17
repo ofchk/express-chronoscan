f94d7694e9a5f0142913a2c21f4817131538593d
sarah <sarah@ofchk.biz>
2023-05-17 13:20:18 +0530

    oracle returning id added ROECORD_ID

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 var throttle = require('express-throttle-bandwidth');
-var cron = require('node-cron');
+
 const oracledb = require('oracledb');
 
 const dbConfig = require('./dbconfig.js');
@@ -94,7 +94,7 @@ async function connect_oracle_staging(invoice_number, vendor_name, site_id, curr
 //    result = await connection.execute(sql);
 //    console.log("Number of rows inserted:", result);
 
-   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE, ATTRIBUTE9) VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9) RETURNING RECORD_ID INTO staging_id`;
+   sql = `INSERT INTO "XXMO_DMS"."XXMO_DMS_AP_INVOICE_STG_T" (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE, ATTRIBUTE9) VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9) `;
 
 
     binds = [
