ce3814ff476048ce0806412e72fc9bd366cf6243
sarah <sarah@ofchk.biz>
2023-06-09 13:46:32 +0530

    added userSearchBase in LDAP

--- index.js
+++ index.js
@@ -514,7 +514,7 @@ app.post('/user/login', async (req, res) => {
         username: email,
         usernameAttribute: 'userPrincipalName',
         attributed: ['dn','sAMAccountName'],
-        userDn: 'OU=dmsgroup,OU=MOG_CORP_Finance,OU=MOG_CORP_WITHOUT-USB,OU=MOG-CORP-GROUP,DC=moc,DC=com',
+        userSearchBase: 'OU=dmsgroup,OU=MOG_CORP_Finance,OU=MOG_CORP_WITHOUT-USB,OU=MOG-CORP-GROUP,DC=moc,DC=com',
 
       };
 
