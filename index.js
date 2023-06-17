481dff1fa51b6b5e0edc8cc234a44d4bc2cb7ff8
sarah <sarah@ofchk.biz>
2023-06-17 07:41:11 +0530

    mnt path

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const Role = db.role;
 db.sequelize.sync();
 
 var pathFrom = `${__dirname}/uploads`; // Or wherever your files-to-process live
-var pathTo = `/mnt/windows/imported`;
+var pathTo = `/mnt`;
 
 console.log('Today', new Date())
 app.get('/', (req, res) => {
