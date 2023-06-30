0e4f85d2bb1c170f763fce8d0e3b88ae08d1d535
sarah <sarah@ofchk.biz>
2023-06-30 19:02:53 +0530

    line items fix - error fix

--- index.js
+++ index.js
@@ -403,7 +403,6 @@ async function connect_oracle_staging_header_only(invoice_id, params, header_arr
 
     const invoice_main = params.data.invoice
 
-    console.log('i',i)
     sql = "INSERT INTO XXMO_DMS_AP_INVOICE_STG_T (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE, ATTRIBUTE9, PO_NUMBER, ORG_ID, VENDOR_CODE, LINE_CURRENCY) VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13)";
 
      const qty = parseFloat(invoice_items[i].qty) || 0
@@ -460,7 +459,7 @@ async function connect_oracle_staging_header_line_items(invoice_id, params, head
 
      const qty = parseFloat(line_items_array.line_items[i].qty) || 0
      const price = parseFloat(line_items_array.line_items[i].rate) || 0
-      binds = [invoice_main[0].invoice_number, invoice_main[0].invoice_vendor.supplier_name, invoice_main[0].invoice_vendor.site_code, invoice_main[0].invoice_currency.title, invoice_main[0].invoice_entity.title, parseFloat(invoice_main[0].invoice_amount), new Date(invoice_main[0].gl_date), new Date(invoice_main[0].gl_date), invoice_main[0].invoice_files[0].alfresco_url, header_array.LPO, line_items_array.line_items[i].item, qty, price, invoice_main[0].invoice_entity.org_id,line_items_array.line_items[i].slno,, invoice_main[0].invoice_vendor.supplier_number,invoice_main[0].invoice_currency.title ];    
+      binds = [invoice_main[0].invoice_number, invoice_main[0].invoice_vendor.supplier_name, invoice_main[0].invoice_vendor.site_code, invoice_main[0].invoice_currency.title, invoice_main[0].invoice_entity.title, parseFloat(invoice_main[0].invoice_amount), new Date(invoice_main[0].gl_date), new Date(invoice_main[0].gl_date), invoice_main[0].invoice_files[0].alfresco_url, header_array.LPO, line_items_array.line_items[i].item, qty, price, invoice_main[0].invoice_entity.org_id,line_items_array.line_items[i].slno, invoice_main[0].invoice_vendor.supplier_number,invoice_main[0].invoice_currency.title ];    
       
       options = {
         autoCommit: true,
