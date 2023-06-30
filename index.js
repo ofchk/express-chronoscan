fb3e742e964ebdc0045844455ee69b7d15c1ed3e
sarah <sarah@ofchk.biz>
2023-06-30 19:09:11 +0530

    header only fix - error fix

--- index.js
+++ index.js
@@ -405,22 +405,20 @@ async function connect_oracle_staging_header_only(invoice_id, params, header_arr
 
     sql = "INSERT INTO XXMO_DMS_AP_INVOICE_STG_T (INVOICE_NUM, VENDOR_NAME, VENDOR_SITE_ID, HEADER_CURRENCY, OPERATING_UNIT, ENTERED_AMOUNT, GL_DATE, INVOICE_DATE, ATTRIBUTE9, PO_NUMBER, ORG_ID, VENDOR_CODE, LINE_CURRENCY) VALUES (:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13)";
 
-     const qty = parseFloat(invoice_items[i].qty) || 0
-     const price = parseFloat(invoice_items[i].price) || 0
-      binds = [invoice_main[0].invoice_number, invoice_main[0].invoice_vendor.supplier_name, invoice_main[0].invoice_vendor.site_code, invoice_main[0].invoice_currency.title, invoice_main[0].invoice_entity.title, parseFloat(invoice_main[0].invoice_amount), new Date(invoice_main[0].gl_date), new Date(invoice_main[0].gl_date), invoice_main[0].invoice_files[0].alfresco_url, header_array.LPO, invoice_main[0].invoice_entity.org_id, invoice_main[0].invoice_vendor.supplier_number,invoice_main[0].invoice_currency.title ];    
+    binds = [invoice_main[0].invoice_number, invoice_main[0].invoice_vendor.supplier_name, invoice_main[0].invoice_vendor.site_code, invoice_main[0].invoice_currency.title, invoice_main[0].invoice_entity.title, parseFloat(invoice_main[0].invoice_amount), new Date(invoice_main[0].gl_date), new Date(invoice_main[0].gl_date), invoice_main[0].invoice_files[0].alfresco_url, header_array.LPO, invoice_main[0].invoice_entity.org_id, invoice_main[0].invoice_vendor.supplier_number,invoice_main[0].invoice_currency.title ];    
       
 
-      options = {
-        autoCommit: true,
-        outFormat: oracledb.OUT_FORMAT_OBJECT,      
-      };
+    options = {
+      autoCommit: true,
+      outFormat: oracledb.OUT_FORMAT_OBJECT,      
+    };
 
-      result = await connection.execute(
-                  sql,
-                  binds,
-                  options);
-      console.log("Inserted Row ID:", result.lastRowid);
-      await save_staging(invoice_id, result.lastRowid)
+    result = await connection.execute(
+                sql,
+                binds,
+                options);
+    console.log("Inserted Row ID:", result.lastRowid);
+    await save_staging(invoice_id, result.lastRowid)
  
 
   } catch (err) {
